b92cb42f25a41fe2352f22cc856042d44a499e86
iAlexander <alexander@iashchuk.com>
2017-11-08 19:07:28 +0200

    Initial commit from Xcode

---
+++ PlumberSheet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlumberSheet
+//
+//  Created by Alexander Iashchuk on 8/11/17.
+//  Copyright © 2017 Alexander Iashchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PlumberSheetUITests/PlaygroundStrings.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var input = "Vde"
+
+//var twoLetters = String(input.characters.prefix(2))
+
+var twoLetters = String(input.prefix(2))
+
+let first = String(twoLetters.prefix(1))
+let second = String(twoLetters.suffix(1))
+
+
+
+
+
