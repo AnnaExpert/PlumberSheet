537d60db021a1a37b757f2a3ffb68ac51ffa07ee
iAlexander <alexander@iashchuk.com>
2017-11-08 21:23:19 +0200

    Added navigation and detail screen

--- PlumberSheet/SheetViewController.swift
+++ PlumberSheet/SheetViewController.swift
@@ -1,6 +1,6 @@
 //
 //  http://iashchuk.com
-//  ViewController.swift
+//  SheetViewController.swift
 //  PlumberSheet - Work sheet app for in-house plumber engineers
 //
 //  Created by Alexander Iashchuk on 3/11/17.
@@ -9,7 +9,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SheetViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
