93e126fa52abb05cbccdb37440d02bf11a0090ac
iAlexander <alexander@iashchuk.com>
2017-11-20 02:00:08 +0200

    Removed all specimen supporting files and updated the application navigation

--- PlumberSheet/MainViewController.swift
+++ PlumberSheet/MainViewController.swift
@@ -7,9 +7,10 @@
 //  Copyright © 2017 Alexander Iashchuk (iAlexander). All rights reserved.
 //
 
+import UIKit
 import Eureka
-/*
-class MainFormViewController: FormViewController {
+
+class MainViewController: FormViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,89 +29,4 @@ class MainFormViewController: FormViewController {
                 $0.value = Date(timeIntervalSinceReferenceDate: 0)
         }
     }
-}
-*/
-//MARK: MainViewController
-
-class MainViewController : FormViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        form +++
-            
-            Section("iAlexander Section")
-            
-            <<< ButtonRow("Rows") {
-                $0.title = $0.tag
-                $0.presentationMode = .segueName(segueName: "RowsExampleViewControllerSegue", onDismiss: nil)
-            }
-            
-            <<< ButtonRow("Native iOS Event Form") { row in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "NativeEventsFormNavigationControllerSegue", onDismiss:{  vc in vc.dismiss(animated: true) })
-            }
-            
-            <<< ButtonRow("Accesory View Navigation") { (row: ButtonRow) in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "AccesoryViewControllerSegue", onDismiss: nil)
-            }
-            
-            <<< ButtonRow("Custom Cells") { (row: ButtonRow) -> () in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "CustomCellsControllerSegue", onDismiss: nil)
-            }
-            
-            <<< ButtonRow("Customization of rows with text input") { (row: ButtonRow) -> Void in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "FieldCustomizationControllerSegue", onDismiss: nil)
-            }
-            
-            <<< ButtonRow("Hidden rows") { (row: ButtonRow) -> Void in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "HiddenRowsControllerSegue", onDismiss: nil)
-            }
-            
-            <<< ButtonRow("Disabled rows") { (row: ButtonRow) -> Void in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "DisabledRowsControllerSegue", onDismiss: nil)
-            }
-            
-            <<< ButtonRow("Formatters") { (row: ButtonRow) -> Void in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "FormattersControllerSegue", onDismiss: nil)
-            }
-            
-            <<< ButtonRow("Inline rows") { (row: ButtonRow) -> Void in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "InlineRowsControllerSegue", onDismiss: nil)
-            }
-            <<< ButtonRow("List Sections") { (row: ButtonRow) -> Void in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "ListSectionsControllerSegue", onDismiss: nil)
-            }
-            <<< ButtonRow("Validations") { (row: ButtonRow) -> Void in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "ValidationsControllerSegue", onDismiss: nil)
-            }
-            
-            <<< ButtonRow("Custom Design") { (row: ButtonRow) -> Void in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "CustomDesignControllerSegue", onDismiss: nil)
-            }
-            
-            <<< ButtonRow("Multivalued Sections") { (row: ButtonRow) -> Void in
-                row.title = row.tag
-                row.presentationMode = .segueName(segueName: "MultivaluedSectionsControllerSegue", onDismiss: nil)
-            }
-            
-            
-            +++ Section()
-            <<< ButtonRow() { (row: ButtonRow) -> Void in
-                row.title = "About"
-                }
-                .onCellSelection { [weak self] (cell, row) in
-//                    self?.showAlert()
-        }
-}
 }
