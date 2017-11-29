6353886002511bcece1a6ee00f3d4d1c60ffba46
iAlexander <alexander@iashchuk.com>
2017-11-29 17:48:09 +0200

    Updated the form fields UI

---
+++ PlumberSheet/JobTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  JobTableViewCell.swift
+//  PlumberSheet - Work sheet app for in-house plumber engineers
+//
+//  Created by Alexander Iashchuk on 28/11/17.
+//  Copyright © 2017 Alexander Iashchuk (iAlexander). All rights reserved.
+//
+
+import UIKit
+
+class JobTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
