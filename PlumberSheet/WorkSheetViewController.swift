6dc3eedb9029de491c7a9dd084a5e62c3d2dd9af
iAlexander <alexander@iashchuk.com>
2017-11-30 04:41:18 +0200

    Added new view controller

---
+++ PlumberSheet/WorkSheetViewController.swift
@@ -0,0 +1,36 @@
+//
+//  http://iashchuk.com
+//  WorkSheetViewController.swift
+//  PlumberSheet - Work sheet app for in-house plumber engineers
+//
+//  Created by Alexander Iashchuk on 29/11/17.
+//  Copyright © 2017 Alexander Iashchuk (iAlexander). All rights reserved.
+//
+
+import UIKit
+
+class WorkSheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
