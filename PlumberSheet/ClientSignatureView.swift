5fb7dd29fd787610617162b4fe5aa7a493e3193d
iAlexander <alexander@iashchuk.com>
2017-12-11 01:39:39 +0200

    Finished the signature view class and prepared project for feature testing

--- PlumberSheet/ClientSignatureView.swift
+++ PlumberSheet/ClientSignatureView.swift
@@ -3,7 +3,7 @@
 //  http://iashchuk.com
 //  ClientSignatureView.swift
 //  PlumberSheet - Work sheet app for in-house plumber engineers
-//  Application version 0.7.1, build 1
+//  Application version 0.7.1, build 3
 //  Last modification on 2017.12.11
 //
 //   App precisely handcrafted in KyivApp Development Studio
@@ -15,7 +15,7 @@
 
 
 import UIKit
-
+Finished the signature view class
 public struct Signature {
     
     private(set) public var image : UIImage
@@ -26,7 +26,6 @@ public struct Signature {
         self.date = Date()
     }
 }
-
 
 public protocol SignatureViewDelegate: class {
     func SignatureViewDidCaptureSignature(view: SignatureView, signature: Signature?)
@@ -56,193 +55,5 @@ extension SignatureViewDelegate {
     
     func SignatureViewDidCancelDrawing(view: SignatureView) {
         //optional
-    }
-}
-
-// MARK: - Class Decleration
-
-@IBDesignable
-open class SignatureView: UIView {
-    
-    // MARK: - Public properties
-    
-    @IBInspectable public var lineColor              : UIColor   = UIColor.black
-    @IBInspectable public var lineWidth              : CGFloat   = 3.0
-    @IBInspectable public var lineOpacity            : CGFloat   = 1.0
-    @IBInspectable public var drawingEnabled         : Bool      = true
-    @IBInspectable public var signatureIsOpaque      : Bool      = true
-    public weak var delegate                         : SignatureViewDelegate?
-    public var signaturePresent: Bool {
-        if pathArray.isEmpty {
-            return false
-        } else {
-            return true
-        }
-    }
-    
-    // MARK: - Private properties
-    
-    private var pathArray                            : [Line]    = []
-    private var currentPoint                         : CGPoint   = CGPoint()
-    private var previousPoint                        : CGPoint   = CGPoint()
-    private var previousPreviousPoint                : CGPoint   = CGPoint()
-    
-    private struct Line {
-        var path    : CGMutablePath
-        var color   : UIColor
-        var width   : CGFloat
-        var opacity : CGFloat
-        
-        init(path : CGMutablePath, color: UIColor, width: CGFloat, opacity: CGFloat) {
-            self.path    = path
-            self.color   = color
-            self.width   = width
-            self.opacity = opacity
-        }
-    }
-    
-    override public init(frame: CGRect) {
-        super.init(frame: frame)
-        backgroundColor = UIColor.clear
-    }
-    
-    required public init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        backgroundColor = UIColor.clear
-    }
-    
-    override open func draw(_ rect: CGRect) {
-        let context : CGContext = UIGraphicsGetCurrentContext()!
-        context.setLineCap(.round)
-        
-        for line in pathArray {
-            context.setLineWidth(line.width)
-            context.setAlpha(line.opacity)
-            context.setStrokeColor(line.color.cgColor)
-            context.addPath(line.path)
-            context.beginTransparencyLayer(auxiliaryInfo: nil)
-            context.strokePath()
-            context.endTransparencyLayer()
-        }
-    }
-    
-    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard drawingEnabled == true else {
-            return
-        }
-        
-        delegate?.SignatureViewDidBeginDrawing(view: self)
-        if let touch = touches.first as UITouch! {
-            setTouchPoints(touch, view: self)
-            let newLine = Line(path: CGMutablePath(), color: lineColor, width: lineWidth, opacity: lineOpacity)
-            newLine.path.addPath(createNewPath())
-            pathArray.append(newLine)
-        }
-    }
-    
-    override open func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard drawingEnabled == true else {
-            return
-        }
-        
-        delegate?.SignatureViewIsDrawing(view: self)
-        if let touch = touches.first as UITouch! {
-            updateTouchPoints(touch, view: self)
-            let newLine = createNewPath()
-            if let currentPath = pathArray.last {
-                currentPath.path.addPath(newLine)
-            }
-        }
-    }
-    
-    override open func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard drawingEnabled == true else {
-            return
-        }
-        delegate?.SignatureViewDidFinishDrawing(view: self)
-    }
-    
-    override open func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard drawingEnabled == true else {
-            return
-        }
-        delegate?.SignatureViewDidCancelDrawing(view: self)
-    }
-    
-    public func clearCanvas() {
-        pathArray = []
-        setNeedsDisplay()
-    }
-    
-    public func captureSignature() {
-        guard signaturePresent != false else {
-            delegate?.SignatureViewDidCaptureSignature(view: self, signature: nil)
-            return
-        }
-        
-        let signatureImage = captureSignatureFromView()
-        if let image = signatureImage {
-            delegate?.SignatureViewDidCaptureSignature(view: self, signature: Signature(signature: image))
-        } else {
-            delegate?.SignatureViewDidCaptureSignature(view: self, signature: nil)
-        }
-    }
-    
-    // MARK: - Private Functions
-    
-    private func captureSignatureFromView() -> UIImage? {
-        UIGraphicsBeginImageContextWithOptions(bounds.size, signatureIsOpaque, 0.0)
-        
-        guard let context = UIGraphicsGetCurrentContext() else {
-            return nil
-        }
-        
-        layer.render(in: context)
-        let viewImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return viewImage
-    }
-    
-    private func setTouchPoints(_ touch: UITouch,view: UIView) {
-        previousPoint = touch.previousLocation(in: view)
-        previousPreviousPoint = touch.previousLocation(in: view)
-        currentPoint = touch.location(in: view)
-    }
-    
-    private func updateTouchPoints(_ touch: UITouch,view: UIView) {
-        previousPreviousPoint = previousPoint
-        previousPoint = touch.previousLocation(in: view)
-        currentPoint = touch.location(in: view)
-    }
-    
-    private func createNewPath() -> CGMutablePath {
-        let midPoints = getMidPoints()
-        let subPath = createSubPath(midPoints.0, mid2: midPoints.1)
-        let newPath = addSubPathToPath(subPath)
-        return newPath
-    }
-    
-    private func calculateMidPoint(_ p1 : CGPoint, p2 : CGPoint) -> CGPoint {
-        return CGPoint(x: (p1.x + p2.x) * 0.5, y: (p1.y + p2.y) * 0.5);
-    }
-    
-    private func getMidPoints() -> (CGPoint,  CGPoint) {
-        let mid1 : CGPoint = calculateMidPoint(previousPoint, p2: previousPreviousPoint)
-        let mid2 : CGPoint = calculateMidPoint(currentPoint, p2: previousPoint)
-        return (mid1, mid2)
-    }
-    
-    private func createSubPath(_ mid1: CGPoint, mid2: CGPoint) -> CGMutablePath {
-        let subpath : CGMutablePath = CGMutablePath()
-        subpath.move(to: CGPoint(x: mid1.x, y: mid1.y))
-        subpath.addQuadCurve(to: CGPoint(x: mid2.x, y: mid2.y), control: CGPoint(x: previousPoint.x, y: previousPoint.y))
-        return subpath
-    }
-    
-    private func addSubPathToPath(_ subpath: CGMutablePath) -> CGMutablePath {
-        let bounds : CGRect = subpath.boundingBox
-        let drawBox : CGRect = bounds.insetBy(dx: -2.0 * lineWidth, dy: -2.0 * lineWidth)
-        setNeedsDisplay(drawBox)
-        return subpath
     }
 }
