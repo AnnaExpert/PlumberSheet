8331f9b3b1a23c14eacfc94c587a670697a24733
iAlexander <alexander@iashchuk.com>
2017-12-11 01:31:19 +0200

    Updated the signature view controller

--- PlumberSheet/ClientSignatureView.swift
+++ PlumberSheet/ClientSignatureView.swift
@@ -1,20 +1,21 @@
-/*Copyright (c) 2016, Alexander Iashchuk.
- 
- Redistribution and use in source and binary forms, with or without modification,are permitted provided that the following conditions are met:
- 
- 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- 
- 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
- BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+//
+//  http://kyivapp.com
+//  http://iashchuk.com
+//  ClientSignatureView.swift
+//  PlumberSheet - Work sheet app for in-house plumber engineers
+//  Application version 0.7.1, build 1
+//  Last modification on 2017.12.11
+//
+//   App precisely handcrafted in KyivApp Development Studio
+//  File created by Alexander Iashchuk on 10/12/17
+//  Copyright © 2017 iashchuk.com
+//  Alexander Iashchuk (iAlexander)
+//  All rights reserved
+//
+
 
 import UIKit
-
+//Finished the signature view class
 public struct Signature {
     
     private(set) public var image : UIImage
